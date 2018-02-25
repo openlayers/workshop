7b1f491f8ed744874d2521523be3eddd7e1668e1
Matt Walker <walkermatt@longwayaround.org.uk>
2018-02-25 10:34:55 +0000

    ol-hashed requires access to map variable

--- src/en/examples/vector/geojson.js
+++ src/en/examples/vector/geojson.js
@@ -5,7 +5,7 @@ import VectorLayer from 'ol/layer/vector';
 import VectorSource from 'ol/source/vector';
 import View from 'ol/view';
 
-new Map({
+const map = new Map({
   target: 'map-container',
   layers: [
     new VectorLayer({
