c0ae05b960b68f12f7a69052241b5a97f56116c1
Tim Schaub <tim.schaub@gmail.com>
2018-05-08 17:30:08 -0600

    Updated dev setup

--- src/en/webpack.config.js
+++ src/en/webpack.config.js
@@ -1,100 +1,31 @@
-const BabiliPlugin = require('babili-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const Dashboard = require('webpack-dashboard/plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const HtmlPlugin = require('html-webpack-plugin');
-const merge = require('webpack-merge');
 const path = require('path');
-const webpack = require('webpack');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
 
-const main = {
-  context: __dirname,
-  target: 'web',
-  entry: {
-    main: './main.js'
+module.exports = {
+  entry: './main.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'main.js'
+  },
+  devtool: 'source-map',
+  devServer: {
+    port: 3000,
+    clientLogLevel: 'none',
+    stats: 'errors-only'
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
         test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader'
-        })
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
   plugins: [
     new CopyPlugin([{from: 'data', to: 'data'}]),
-    new ExtractTextPlugin('style.css'),
     new HtmlPlugin({
       template: 'index.html'
     })
   ]
-};
-
-// configuration specific to the dev environment
-const dev = {
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'build'),
-    pathinfo: true
-  },
-  devtool: 'source-map',
-  plugins: [
-    new Dashboard({
-      color: 'cyan'
-    }),
-    new webpack.EnvironmentPlugin(
-      Object.assign({
-        NODE_ENV: 'development'
-      })
-    )
-  ],
-  devServer: {
-    host: '0.0.0.0',
-    port: 3000,
-    disableHostCheck: true,
-    overlay: true
-  }
-};
-
-// configuration specific to the prod environment
-const prod = {
-  output: {
-    filename: '[name].[chunkhash].js',
-    path: path.resolve(__dirname, 'build')
-  },
-  devtool: 'source-map',
-  plugins: [
-    new webpack.EnvironmentPlugin(
-      Object.assign({
-        NODE_ENV: 'production'
-      })
-    ),
-    new BabiliPlugin()
-  ]
-};
-
-module.exports = env => {
-  let config;
-
-  switch (env) {
-    case 'prod': {
-      config = merge(main, prod);
-      break;
-    }
-    default: {
-      config = merge(main, dev);
-    }
-  }
-
-  return config;
 };
