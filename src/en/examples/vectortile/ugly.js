3f88e7839ef18a47ff298b82e72935ff2dc96e5c
ahocevar <andreas.hocevar@gmail.com>
2018-05-13 16:34:05 -0500

    Update default style function

--- src/en/examples/vectortile/ugly.js
+++ src/en/examples/vectortile/ugly.js
@@ -7,6 +7,7 @@ import VectorTileSource from 'ol/source/VectorTile';
 import Overlay from 'ol/Overlay';
 //! [style-import]
 import {Style, Fill, Stroke, Circle, Text} from 'ol/style';
+// import createDefaultStyle from 'ol/style/Style';
 //! [style-import]
 
 const key = 'lirfd6Fegsjkvs0lshxe';
@@ -146,6 +147,90 @@ layer.setStyle(function(feature, resolution) {
     return point;
   }
 
-  //return Style.defaultFunction(feature, resolution);
+  //layer.setStyle(function(feature, resolution) {
+  const properties = feature.getProperties();
+
+  // Water polygons
+  if (properties.layer == 'water') {
+    return new Style({
+      fill: new Fill({
+        color: 'rgba(0, 0, 255, 0.7)'
+      })
+    });
+  }
+
+  // Boundary lines
+  if (properties.layer == 'boundary' && properties.admin_level == 2) {
+    return new Style({
+      stroke: new Stroke({
+        color: 'gray'
+      })
+    });
+  }
+
+  // Continent labels
+  if (properties.layer == 'place' && properties.class == 'continent') {
+    return new Style({
+      text: new Text({
+        text: properties.name,
+        font: 'bold 16px Open Sans',
+        fill: new Fill({
+          color: 'black'
+        }),
+        stroke: new Stroke({
+          color: 'white'
+        })
+      })
+    });
+  }
+
+  // Country labels
+  if (properties.layer == 'place' && properties.class == 'country' &&
+      resolution < map.getView().getResolutionForZoom(5)) {
+    return new Style({
+      text: new Text({
+        text: properties.name,
+        font: 'normal 13px Open Sans',
+        fill: new Fill({
+          color: 'black'
+        }),
+        stroke: new Stroke({
+          color: 'white'
+        })
+      })
+    });
+  }
+
+  // Capital icons and labels
+  if (properties.layer == 'place' && properties.capital) {
+    const point = new Style({
+      image: new Circle({
+        radius: 5,
+        fill: new Fill({
+          color: 'black'
+        }),
+        stroke: new Stroke({
+          color: 'gray'
+        })
+      })
+    });
+
+    if (resolution < map.getView().getResolutionForZoom(6)) {
+      point.setText(new Text({
+        text: properties.name,
+        font: 'italic 12px Open Sans',
+        offsetY: -12,
+        fill: new Fill({
+          color: '#013'
+        }),
+        stroke: new Stroke({
+          color: 'white'
+        })
+      }));
+    }
+    return point;
+  }
+
+  //return createDefaultStyle(feature, resolution)
 });
 //! [style]
