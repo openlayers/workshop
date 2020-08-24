8734d5f7da0af839b74b4bf69dbb9f0519517b46
ca0v <coreyalix@hotmail.com>
2020-08-24 18:43:37 -0400

    adds basic example for typescript

---
+++ src/en/examples/typescript/map.ts
@@ -0,0 +1,20 @@
+import "ol/ol.css";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import XYZSource from "ol/source/XYZ";
+import { fromLonLat } from "ol/proj";
+
+new Map({
+  target: "map-container",
+  layers: [
+    new TileLayer({
+      source: new XYZSource({
+        url: "http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg",
+      }),
+    }),
+  ],
+  view: new View({
+    center: fromLonLat([0, 0]),
+    zoom: 2,
+  }),
+});
